db6e483d23371b89c30ddb021b54519cad4eeddb
Galeh-ariya <galehariya8@gmail.com>
2024-06-19 10:47:54 +0700

    layouting navbar and sidebar

---
+++ resources/views/components/Layout/layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Calendar | Minia - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Tailwind Admin & Dashboard Template" name="description">
+    <meta content="Themesbrand" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="./assets/img/favicon.ico">
+    <!-- Layout config Js -->
+    <!-- Icons CSS -->
+    @vite('resources/css/app.css')
+
+
+    <!-- Tailwind CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/tailwind2.css')}}">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+</head>
+
+<body data-mode="light" data-sidebar-size="lg" class="group">
+
+    <x-Layout.sidebar />
+    <x-Layout.navbar />
+
+    {{ $slot }}
+
+    <script src="{{ asset('/assets/libs/@popperjs/core/umd/popper.min.js') }}"></script>
+    <script src="./assets/libs/feather-icons/feather.min.js"></script>
+    <script src="./assets/libs/metismenujs/metismenujs.min.js"></script>
+    <script src="./assets/libs/simplebar/simplebar.min.js"></script>
+
+    <!-- plugin js -->
+    <!-- <script src="assets/libs/fullcalendar/index.global.min.js"></script> -->
+
+    <!-- calendar init -->
+     <!-- <script src="assets/js/pages/calendar.init.js"></script>  -->
+
+    <script src="{{ asset('./assets/js/app.js') }}"></script>
+</body>
+
+</html>

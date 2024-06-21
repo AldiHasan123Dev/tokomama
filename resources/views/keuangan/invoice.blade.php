99a3e1ea2aa405581b2580deb077c1a3ec62f5ff
NandaDwiCahyo <nandad.cahyo@gmail.com>
2024-06-21 15:13:56 +0700

    Membuat view menu invoice

--- resources/views/keuangan/invoice.blade.php
+++ resources/views/keuangan/invoice.blade.php
@@ -1,3 +1,23 @@
 <x-Layout.layout>
-    <h1>Test</h1>
+    <div class="card bg-base-100 shadow-xl">
+        <div class="card-body">
+            <div class="grid grid-cols-5">
+                <div>
+                    <button class="btn btn-sm bg-green-400 text-white font-bold w-fit">Edit tanggal</button>
+                </div>
+                <div>
+                    <p>List Semua Invoice </p>
+                </div>
+                <div>
+                    <p>INVOICE (selected): </p>
+                </div>
+                <div>
+                    <button class="btn btn-sm bg-blue-400 text-white font-bold w-fit">Rekap Invoice Excel</button>
+                </div>
+                <div>
+                    <button class="btn btn-sm bg-green-400 text-white font-bold w-fit"><i class="fas fa-print"></i> Cetak Invoice Ulang</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-Layout.layout>

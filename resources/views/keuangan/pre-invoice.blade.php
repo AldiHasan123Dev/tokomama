aa4627700dc64a30464960dbaed98983f4ee84f9
Spyder-code <luaysyauqillah@gmail.com>
2024-07-02 20:42:39 +0700

    fix datatable assets

--- resources/views/keuangan/pre-invoice.blade.php
+++ resources/views/keuangan/pre-invoice.blade.php
@@ -26,29 +26,32 @@
           </div>
     </x-keuangan.card-keuangan>
 
-    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
-    <script>
-        let table = $(`#table-getfaktur`).DataTable({
-            ajax: {
-                url: "{{route('invoice.pre-invoice')}}",
-                dataSrc: "data"
-            },
-            columns: [
-                { data: 'aksi', name: 'aksi' },
-                { data: 'DT_RowIndex', name: 'number'},
-                { data: 'nomor_surat', name: 'No. Surat' },
-                { data: 'kepada', name: 'kepada' },
-                { data: 'jumlah', name: 'jumlah' },
-                { data: 'satuan', name: 'satuan' },
-                { data: 'jenis_barang', name: 'jenis_barang' },
-                { data: 'nama_kapal', name: 'nama_kapal' },
-                { data: 'no_cont', name: 'no_cont' },
-                { data: 'no_seal', name: 'no_seal' },
-                { data: 'no_pol', name: 'no_pol' },
-                { data: 'tujuan', name: 'tujuan' },
-                { data: 'id', name: 'id', visible:false},
-                
-            ]
-        });
-    </script>
+    {{-- <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script> --}}
+    <x-slot:script>
+        <script>
+            let table = $(`#table-getfaktur`).DataTable({
+                ajax: {
+                    url: "{{route('invoice.pre-invoice')}}",
+                    dataSrc: "data"
+                },
+                columns: [
+                    { data: 'aksi', name: 'aksi' },
+                    { data: 'DT_RowIndex', name: 'number'},
+                    { data: 'nomor_surat', name: 'No. Surat' },
+                    { data: 'kepada', name: 'kepada' },
+                    { data: 'jumlah', name: 'jumlah' },
+                    { data: 'satuan', name: 'satuan' },
+                    { data: 'jenis_barang', name: 'jenis_barang' },
+                    { data: 'nama_kapal', name: 'nama_kapal' },
+                    { data: 'no_cont', name: 'no_cont' },
+                    { data: 'no_seal', name: 'no_seal' },
+                    { data: 'no_pol', name: 'no_pol' },
+                    { data: 'tujuan', name: 'tujuan' },
+                    { data: 'id', name: 'id', visible:false},
+                    
+                ]
+            });
+        </script>
+
+    </x-slot:script>
 </x-Layout.layout>

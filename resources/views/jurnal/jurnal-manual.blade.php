b62db90f7859511c085420de3e2ed9dc9f924eb5
NandaDwiCahyo <nandad.cahyo@gmail.com>
2024-07-31 11:51:52 +0700

    Membuat kondisi checkbox

--- resources/views/jurnal/jurnal-manual.blade.php
+++ resources/views/jurnal/jurnal-manual.blade.php
@@ -9,7 +9,7 @@
             border: 1px solid black;
             border-collapse: collapse;
         }
-        
+
         #param td{
             border: 1px solid black;
             border-collapse: collapse;
@@ -73,7 +73,7 @@
                             </select>
                         </label>
                     </div>
-                
+
                     <div class="w-full">
                         <label class="form-control w-full max-w-xs mb-5">
                             <div class="label">
@@ -105,8 +105,8 @@
                     <tbody id="tableBody">
                         <tr>
                             <td>
-                                <input type="hidden" name="check[0]" id="check[0]" value="0" checked>
-                                <input type="checkbox" name="check[0]" id="check[0]" value="1" checked>
+                                <input type="hidden" name="check[0]" id="check0h" value="0" checked>
+                                <input type="checkbox" name="check[0]" id="check0" value="1" checked>
                             </td>
                             <td>
                                 <select class="select select-bordered w-full max-w-xs" name="invoice[]" id="invoice-1">
@@ -181,7 +181,28 @@
             $(`#td`).text(totaltdtc);
             $(`#tc`).text(totaltdtc);
         });
-        bindInvoiceChange(1); 
+
+        $('#check0').click(function() {
+            if ($('#check0').is(':checked')) {
+                $('#invoice-1').prop('disabled', false);
+                $('#nopol-1').prop('disabled', false);
+                $('#akun_debet-1').prop('disabled', false);
+                $('#akun_kredit-1').prop('disabled', false);
+                $('#keterangan-1').prop('disabled', false);
+                $('#nominal-1').prop('disabled', false);
+                $('#invoice_external-1').prop('disabled', false);
+            } else {
+                $('#invoice-1').prop('disabled', true);
+                $('#nopol-1').prop('disabled', true);
+                $('#akun_debet-1').prop('disabled', true);
+                $('#akun_kredit-1').prop('disabled', true);
+                $('#keterangan-1').prop('disabled', true);
+                $('#nominal-1').prop('disabled', true);
+                $('#invoice_external-1').prop('disabled', true);
+            }
+        });
+
+        bindInvoiceChange(1);
     });
 
     let no = 1;
@@ -215,8 +236,8 @@
         let html = `
         <tr>
             <td>
-                <input type="hidden" name="check[${newRowId - 1}]" id="check[${newRowId - 1}]" value="0" checked>
-                <input type="checkbox" name="check[${newRowId - 1}]" id="check[${newRowId - 1}]" value="1" checked>
+                <input type="hidden" name="check[${newRowId - 1}]" id="check${newRowId - 1}h" value="0" checked>
+                <input type="checkbox" name="check[${newRowId - 1}]" id="check${newRowId - 1}" value="1" checked>
             </td>
             <td>
                 <select class="select select-bordered w-full max-w-xs" name="invoice[]" id="invoice-${newRowId}">
@@ -289,9 +310,29 @@
             $(`#tc`).text(totaltdtc);
         });
 
+        $(`#check${newRowId - 1}`).click(function() {
+            if ($(`#check${newRowId - 1}`).is(':checked')) {
+                $(`#invoice-${newRowId}`).prop('disabled', false);
+                $(`#nopol-${newRowId}`).prop('disabled', false);
+                $(`#akun_debet-${newRowId}`).prop('disabled', false);
+                $(`#akun_kredit-${newRowId}`).prop('disabled', false);
+                $(`#keterangan-${newRowId}`).prop('disabled', false);
+                $(`#nominal-${newRowId}`).prop('disabled', false);
+                $(`#invoice_external-${newRowId}`).prop('disabled', false);
+            } else {
+                $(`#invoice-${newRowId}`).prop('disabled', true);
+                $(`#nopol-${newRowId}`).prop('disabled', true);
+                $(`#akun_debet-${newRowId}`).prop('disabled', true);
+                $(`#akun_kredit-${newRowId}`).prop('disabled', true);
+                $(`#keterangan-${newRowId}`).prop('disabled', true);
+                $(`#nominal-${newRowId}`).prop('disabled', true);
+                $(`#invoice_external-${newRowId}`).prop('disabled', true);
+            }
+        });
+
         bindInvoiceChange(newRowId);
     });
-    
+
 </script>
 
 </x-Layout.layout>

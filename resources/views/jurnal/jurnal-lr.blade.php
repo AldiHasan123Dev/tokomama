6f6f6db3b4f17dc513869b2b3d7df20714cdb578
NizarAfnd <affandinizar69@gmail.com>
2024-08-01 15:43:44 +0700

    Fitur Laba Rugi

--- resources/views/jurnal/jurnal-lr.blade.php
+++ resources/views/jurnal/jurnal-lr.blade.php
@@ -26,49 +26,283 @@
                     <button class="btn bg-green-10 text-black hover:text-white my-5 py-4 font-bold border-black" id="aktif" type="submit">Nov</button>
                     <button class="btn bg-green-10 text-black hover:text-white my-5 py-4 font-bold border-black" id="aktif" type="submit">Des</button>
                 </div>
-                    <div class="flex items-center">
+                    <!-- <div class="flex items-center">
                         <label for="tahun" class="mr-2">Tahun:</label>
                         <input type="text" id="tahun" name="tahun" class="input input-bordered rounded-lg dark:text-black my-3 py-3">
+                    </div> -->
+                    <div class="flex items-center">
+                        <b class="mr-2">Tahun:  </b>
+                        <select class="form-control-bordered rounded-lg dark:text-black my-3 py-3" wire:model="year" style="width: 70px">
+                            <option value="2023">2023</option>
+                            <option value="2024">2024</option>
+                            <option value="2025">2025</option>
+                            <option value="2026">2026</option>
+                            <option value="2027">2027</option>
+                            <option value="2028">2028</option>
+                            <option value="2029">2029</option>
+                            <option value="2030">2030</option>
+                        </select>
                     </div>
                 </div>
             </div>
 
-                <table class="table" id="coa_table">
-                    <!-- head -->
-                    <thead>
-                        <tr>
-                            <th>No. Akun</th>
-                            <th>Nama Akun</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                </table>
+
+            <div class="flex justify-between">
+                <div class="w-7/12">
+                            <table class="table-auto w-full text-xs">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3" class="px-4 py-2 text-left">A. PENJUALAN USAHA</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- masukkan logic -->
+                                    @foreach($coa1 as $item)
+                                        @php
+                                        
+                                        $total = $totals[$item->id] ?? ['selisih' => 0];
+                                    @endphp
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $item->no_akun }}</td>
+                                        <td class="border px-4 py-2">{{ $item->nama_akun }}</td>
+                                        <td class="border px-4 py-2 text-right">{{ $total['selisih'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- masukkan logic -->
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan="2" class="border px-4 py-2 font-bold">TOTAL</td>
+                                            <td class="border px-4 py-2 text-right font-bold">{{$totalA}}</td>
+                                        </tr>
+                                    </tfoot>
+                                </tbody>
+                            </table>
+                            <table class="table-auto w-full text-xs">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3" class="px-4 py-2 text-left">B. HARGA POKOK PENJUALAN</th>
+                                    </tr>
+                                    </thead>
+                                <tbody>
+                                    <!-- masukkan logic -->
+                                    @foreach($coa2 as $item)
+                                        @php                                        
+                                        $total = $totals[$item->id] ?? ['selisih' => 0];
+                                    @endphp
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $item->no_akun }}</td>
+                                        <td class="border px-4 py-2">{{ $item->nama_akun }}</td>
+                                        <td class="border px-4 py-2 text-right">{{ $total['selisih'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- masukkan logic -->
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan="2" class="border px-4 py-2 font-bold">TOTAL</td>
+                                            <td class="border px-4 py-2 text-right font-bold">{{$totalB}}</td>
+                                        </tr>
+                                    </tfoot>
+                                </tbody>
+                            </table>
+                            <table class="table-auto w-full text-xs">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3" class="px-4 py-2 text-left">C. BIAYA USAHA</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- masukkan logic -->
+                                    @foreach($coa3 as $item)
+                                        @php                                        
+                                        $total = $totals[$item->id] ?? ['selisih' => 0];
+                                    @endphp
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $item->no_akun }}</td>
+                                        <td class="border px-4 py-2">{{ $item->nama_akun }}</td>
+                                        <td class="border px-4 py-2 text-right">{{ $total['selisih'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- masukkan logic -->
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan="2" class="border px-4 py-2 font-bold">TOTAL</td>
+                                            <td class="border px-4 py-2 text-right font-bold">{{$totalC}}</td>
+                                        </tr>
+                                    </tfoot>
+                                </tbody>
+                            </table>
+                            <table class="table-auto w-full text-xs">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3" class="px-4 py-2 text-left">D. BIAYA DEPRESIASI</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- masukkan logic -->
+                                    @foreach($coa4 as $item)
+                                        @php                                        
+                                        $total = $totals[$item->id] ?? ['selisih' => 0];
+                                    @endphp
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $item->no_akun }}</td>
+                                        <td class="border px-4 py-2">{{ $item->nama_akun }}</td>
+                                        <td class="border px-4 py-2 text-right">{{ $total['selisih'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- masukkan logic -->
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan="2" class="border px-4 py-2 font-bold">TOTAL</td>
+                                            <td class="border px-4 py-2 text-right font-bold">{{$totalD}}</td>
+                                        </tr>
+                                    </tfoot>
+                                </tbody>
+                            </table>
+                            <table class="table-auto w-full text-xs">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3" class="px-4 py-2 text-left">E. PENDAPATAN DAN BIAYA LAIN-LAIN</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- masukkan logic -->
+                                    @foreach($coa5 as $item)
+                                        @php                                        
+                                        $total = $totals[$item->id] ?? ['selisih' => 0];
+                                    @endphp
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $item->no_akun }}</td>
+                                        <td class="border px-4 py-2">{{ $item->nama_akun }}</td>
+                                        <td class="border px-4 py-2 text-right">{{ $total['selisih'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- masukkan logic -->
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan="2" class="border px-4 py-2 font-bold">TOTAL</td>
+                                            <td class="border px-4 py-2 text-right font-bold">{{$totalE}}</td>
+                                        </tr>
+                                    </tfoot>
+                                </tbody>
+                            </table>
+                            <table class="table-auto w-full text-xs">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3" class="px-4 py-2 text-left">F. BIAYA KEUANGAN I</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- masukkan logic -->
+                                    @foreach($coa6 as $item)
+                                        @php                                        
+                                        $total = $totals[$item->id] ?? ['selisih' => 0];
+                                    @endphp
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $item->no_akun }}</td>
+                                        <td class="border px-4 py-2">{{ $item->nama_akun }}</td>
+                                        <td class="border px-4 py-2 text-right">{{ $total['selisih'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- masukkan logic -->
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan="2" class="border px-4 py-2 font-bold">TOTAL</td>
+                                            <td class="border px-4 py-2 text-right font-bold">{{$totalF}}</td>
+                                        </tr>
+                                    </tfoot>
+                                </tbody>
+                            </table>
+                            <table class="table-auto w-full mt-3 text-xs">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3" class="px-4 py-2 text-left">G. BIAYA KEUANGAN II</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- masukkan logic -->
+                                    @foreach($coa7 as $item)
+                                        @php                                        
+                                        $total = $totals[$item->id] ?? ['selisih' => 0];
+                                    @endphp
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $item->no_akun }}</td>
+                                        <td class="border px-4 py-2">{{ $item->nama_akun }}</td>
+                                        <td class="border px-4 py-2 text-right">{{ $total['selisih'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- masukkan logic -->
+                                    <tfoot>
+                                        <tr>
+                                            <td colspan="2" class="border px-4 py-2 font-bold">TOTAL</td>
+                                            <td class="border px-4 py-2 text-right font-bold">{{$totalG}}</td>
+                                        </tr>
+                                    </tfoot>
+                                </tbody>
+                            </table>
+                        </div>
+    
+                    
+
+
+
+                        <table class="w-auto h-3/4 text-xs border-collapse border border-gray-300">
+                            <thead class="bg-gray-100">
+                                <tr>
+                                    <th class="text-black font-bold px-2 py-1 text-left">Summarry</th>
+                                    <th class="text-black font-bold px-2 py-1">Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="border-t">
+                                    <td class="px-2 py-1">TOTAL PENJUALAN USAHA</td>
+                                    <td class="px-2 py-1">{{$totalA}}</td>
+                                </tr>
+                                <tr class="border-t ">
+                                    <td class="px-2 py-1">TOTAL HARGA POKOK PENJUALAN</td>
+                                    <td class="px-2 py-1">{{$totalB}}</td>
+                                </tr>
+                                <tr class="border-t bg-gray-50">
+                                    <td class="px-2 py-1">LABA/RUGI KOTOR</td>
+                                    <td class="px-2 py-1">{{$kotor = $totalA - $totalB}}</td>
+                                </tr>
+                                <tr class="border-t">
+                                    <td class="px-2 py-1">TOTAL BIAYA USAHA</td>
+                                    <td class="px-2 py-1">{{$totalC}}</td>
+                                </tr>
+                                <tr class="border-t">
+                                    <td class="px-2 py-1">TOTAL BIAYA PENYUSUTAN</td>
+                                    <td class="px-2 py-1">{{$totalD}}</td>
+                                </tr>
+                                <tr class="border-t bg-gray-50">
+                                    <td class="px-2 py-1">LABA/RUGI USAHA</td>
+                                    <td class="px-2 py-1">{{$usaha = $kotor - $totalC - $totalD}}</td>
+                                </tr>
+                                <tr class="border-t">
+                                    <td class="px-2 py-1">TOTAL PENDAPATAN DAN BIAYA LAIN-LAIN</td>
+                                    <td class="px-2 py-1">{{$totalE}}</td>
+                                </tr>
+                                <tr class="border-t">
+                                    <td class="px-2 py-1">TOTAL BIAYA KEUANGAN I</td>
+                                    <td class="px-2 py-1">{{$totalF}}</td>
+                                </tr>
+                                <tr class="border-t bg-gray-50">
+                                    <td class="px-2 py-1">LABA/RUGI BERSIH SEBELUM PAJAK</td>
+                                    <td class="px-2 py-1">{{$bersih = $usaha - $totalE - $totalF}}</td>
+                                </tr>
+                                <tr class="border-t">
+                                    <td class="px-2 py-1">TOTAL BIAYA KEUANGAN I</td>
+                                    <td class="px-2 py-1">{{$totalF}}</td>
+                                </tr>
+                                <tr class="border-t bg-gray-50">
+                                    <td class="px-2 py-1">LABA/RUGI BERSIH SESUDAH PAJAK</td>
+                                    <td class="px-2 py-1">{{$pajak = $bersih - $totalG}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
             </form>
         </div>
     </x-keuangan.card-keuangan>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/select/2.0.3/js/dataTables.select.js"></script>
-    <script>
-        $(document).ready(function () {
-            var table = $('#coa_table').DataTable({
-                serverSide: true,
-                select:true,
-                ajax: {
-                    url: "{{ route('coa.data') }}",
-                    type: 'POST'
-                },
-                columns: [
-                    { data: 'no_akun' },
-                    { data: 'nama_akun' },
-                    { data: 'status' },
-                ]
-            });
-
-            $('#coa_table tbody').on('click', 'tr', function () {
-                let row =  table.row( this ).data();
-                $('.btn').removeClass('hidden');
-                $('#print').attr('href', "{{ route('invoice.print', ['id' => ':id']) }}".replace(':id', row.id));
-            });
-        });
-    </script>
 </x-Layout.layout>

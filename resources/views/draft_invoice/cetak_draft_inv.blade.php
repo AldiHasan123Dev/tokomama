70d13edafe13214256c6f0be5ca79c3f4af65a09
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2026-07-31 21:16:08 +0700

    update: sales order 4

--- resources/views/draft_invoice/cetak_draft_inv.blade.php
+++ resources/views/draft_invoice/cetak_draft_inv.blade.php
@@ -389,7 +389,7 @@
          margin-left:50px;
         font-size:0.8rem;
         border-collapse:collapse;
-        transform: translateY(-40px);">
+        transform: translateY(-35px);">
                     <tr>
                         <td
                             style="text-align: left;  margin:-100px auto 10; padding:4px 4px 4px 4px; font-style:italic;">
